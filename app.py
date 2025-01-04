43756bf65bd1ebea94ae75b48fa664b1da44fc6e
Guilherme O Chagas <guilhermecchagas1999@gmail.com>
2025-01-04 01:59:48 -0300

    Third commit

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    message = "Clique no botão!"
-    
-    if request.method == 'POST':
-        message = "Você clicou no botão!"
+# Variável para registrar o clique do botão
+button_clicked = False
 
+@app.route('/')
+def home():
+    message = "Clique no botão!"  # Mensagem inicial
     return render_template('index.html', message=message)
+
+@app.route('/click', methods=['POST'])
+def button_click():
+    global button_clicked
+    button_clicked = True  # Registra que o botão foi clicado
+    print("O botão foi clicado no servidor!")  # Imprime a mensagem no servidor
+    return jsonify(status="success", message="Botão clicado!")
 
 if __name__ == '__main__':
     app.run(debug=True)
