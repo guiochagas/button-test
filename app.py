8af689e6f3590310c04a893d38bdc4e5bf9d9937
Guilherme O Chagas <guilhermecchagas1999@gmail.com>
2025-01-04 01:44:14 -0300

    Second commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    message = "Clique no botão!"
+    
+    if request.method == 'POST':
+        message = "Você clicou no botão!"
+
+    return render_template('index.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
